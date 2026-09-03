4c6257a296dbc22c9f69a71e61bf89e2fe429757
rachitsrivastava01 <94031509+rachitsrivastava01@users.noreply.github.com>
2026-09-03 09:13:59 +0530

    Add Terraform configuration for EC2 instance

--- EC2_instance/main.tf
+++ EC2_instance/main.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+required_version = ">=1.16.1"
+}
+  provider "aws" {
+    region = "ap-south-1"
+  }
 
+resource "aws_instance" "My_first_ec2" {
+   ami = "ami-01a00762f46d584a1"
+   instance_type = "t3.micro"
+tags = {
+   Name = "My first terraform instance"
+  }
+}
